43523d4e2d8d38493772a69bb9ea0920915df8cf
kae_kasui <kaekasui_github@icloud.com>
2016-11-04 18:38:04 +0900

    add config

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,8 @@ module NewAccountBook
     # config.i18n.default_locale = :de
     config.i18n.default_locale = :ja
 
+    config.assets.paths << config.root.join('bower_components')
+
     # Do not swallow errors in after_commit/after_rollback callbacks.
     # config.active_record.raise_in_transactional_callbacks = true
 
