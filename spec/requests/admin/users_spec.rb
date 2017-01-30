74b46bc0944c770e05d00250e40b7449866c8172
kae_kasui <kaekasui_github@icloud.com>
2017-01-30 23:51:08 +0900

    Modify the timing to set i18n

--- spec/requests/admin/users_spec.rb
+++ spec/requests/admin/users_spec.rb
@@ -23,7 +23,7 @@ describe 'GET /admin/users', autodoc: true do
 
   context '管理ユーザーとしてログインしている場合' do
     context '1ページ以内のユーザー数の場合' do
-      let(:sign_in_at) { I18n.l(Time.zone.now) }
+      let(:sign_in_at) { Time.zone.now }
       before do
         Timecop.freeze(sign_in_at)
       end
@@ -43,7 +43,7 @@ describe 'GET /admin/users', autodoc: true do
               status: admin_user.human_status_name,
               nickname: admin_user.nickname,
               email: admin_user.email,
-              last_sign_in_at: sign_in_at
+              last_sign_in_at: I18n.l(sign_in_at)
             },
             {
               id: user.id,
