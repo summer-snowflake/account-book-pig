4a79572f4c122bd75e93348098ab423559d86e06
kae_kasui <kaekasui_github@icloud.com>
2017-02-01 23:01:52 +0900

    Add validation for admins

--- spec/models/admin_spec.rb
+++ spec/models/admin_spec.rb
@@ -3,4 +3,9 @@ require 'rails_helper'
 
 RSpec.describe Admin, type: :model do
   it { is_expected.to belong_to(:user) }
+
+  describe 'バリデーション' do
+    it { is_expected.to validate_presence_of(:user_id) }
+    it { is_expected.to validate_uniqueness_of(:user_id) }
+  end
 end
