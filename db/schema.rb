7bdbf71a57245691c40dee2e3fcf2d1aa8abb4f9
kae_kasui <kaekasui_github@icloud.com>
2017-02-02 23:11:50 +0900

    Remove unnecessary column from notices, and Add spec for scope

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170202022635) do
+ActiveRecord::Schema.define(version: 20170202140912) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -134,7 +134,6 @@ ActiveRecord::Schema.define(version: 20170202022635) do
   create_table "notices", force: :cascade do |t|
     t.string   "title"
     t.text     "content"
-    t.string   "post_type"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.date     "post_at"
--- spec/models/notice_spec.rb
+++ spec/models/notice_spec.rb
@@ -2,7 +2,19 @@
 require 'rails_helper'
 
 RSpec.describe Notice, type: :model do
-  it { is_expected.to validate_presence_of(:title) }
-  it { is_expected.to validate_length_of(:title).is_at_most(100) }
-  it { is_expected.to validate_length_of(:content).is_at_most(10_000) }
+  describe 'バリデーション' do
+    it { is_expected.to validate_presence_of(:title) }
+    it { is_expected.to validate_length_of(:title).is_at_most(100) }
+    it { is_expected.to validate_length_of(:content).is_at_most(10_000) }
+  end
+
+  describe '#published' do
+    let!(:publish_notice) { create(:notice, post_at: Time.zone.today) }
+    let!(:unpublish_notice) { create(:notice, post_at: Time.zone.tomorrow) }
+    subject { Notice.all.published }
+
+    it '公開日が今日より前の投稿のみ取得できる' do
+      expect(subject).to eq [publish_notice]
+    end
+  end
 end
