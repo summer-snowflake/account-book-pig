35c7fb12a08e132ac011c45e2adad1898266d432
kae_kasui <kaekasui_github@icloud.com>
2017-02-02 22:32:29 +0900

    Add the unique limitation for admins table

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170202014610) do
+ActiveRecord::Schema.define(version: 20170202022635) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "admins", force: :cascade do |t|
-    t.integer  "user_id"
+    t.integer  "user_id",    null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_admins_on_user_id", using: :btree
+    t.index ["user_id"], name: "index_admins_on_user_id", unique: true, using: :btree
   end
 
   create_table "auths", force: :cascade do |t|
