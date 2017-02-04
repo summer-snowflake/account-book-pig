df3b8f23162e870fc20b796537adbfbeb5a49dd7
kae_kasui <kaekasui_github@icloud.com>
2017-02-04 09:22:03 +0900

    Fix spec, Set the subject for admins

--- spec/models/admin_spec.rb
+++ spec/models/admin_spec.rb
@@ -5,6 +5,9 @@ RSpec.describe Admin, type: :model do
   it { is_expected.to belong_to(:user) }
 
   describe 'バリデーション' do
+    let(:user) { create(:user) }
+    subject { create(:admin, user: user) }
+
     it { is_expected.to validate_presence_of(:user_id) }
     it { is_expected.to validate_uniqueness_of(:user_id) }
   end
