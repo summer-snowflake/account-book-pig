a324cd0419a9349ad6785ee280bb08f5da34d2fb
kae_kasui <kaekasui_github@icloud.com>
2017-02-04 12:18:33 +0900

    Add validation and limitation for categorize_places

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170204030018) do
+ActiveRecord::Schema.define(version: 20170204030651) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -89,10 +89,11 @@ ActiveRecord::Schema.define(version: 20170204030018) do
   end
 
   create_table "categorize_places", force: :cascade do |t|
-    t.integer  "category_id"
-    t.integer  "place_id"
+    t.integer  "category_id", null: false
+    t.integer  "place_id",    null: false
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.index ["category_id", "place_id"], name: "index_categorize_places_on_category_id_and_place_id", unique: true, using: :btree
     t.index ["category_id"], name: "index_categorize_places_on_category_id", using: :btree
     t.index ["place_id"], name: "index_categorize_places_on_place_id", using: :btree
   end
