f4fe3ba46fe8dcd1ea84343dd6ea1f12afabd699
kae_kasui <kaekasui_github@icloud.com>
2017-02-06 00:46:35 +0900

    Add the factory, the validation and the limitation for tagged_records

---
+++ db/migrate/20170205152813_change_columns_to_tagged_records.rb
@@ -0,0 +1,15 @@
+class ChangeColumnsToTaggedRecords < ActiveRecord::Migration[5.0]
+  def up
+    change_column :tagged_records, :tag_id, :integer, null: false, index: true
+    change_column :tagged_records, :record_id, :integer, null: false, index: true
+
+    add_index :tagged_records, [:record_id, :tag_id], unique: true
+  end
+
+  def down
+    change_column :tagged_records, :tag_id, :integer, null: true
+    change_column :tagged_records, :record_id, :integer, null: true
+
+    remove_index :tagged_records, [:record_id, :tag_id]
+  end
+end
