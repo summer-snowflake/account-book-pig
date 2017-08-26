b3473f2650465b2eccd4f9aacadc3c3e4473e066
summer-snow <pig.book.notice@gmail.com>
2017-08-27 07:22:55 +0900

    Modify following rubocop rules

--- app/models/all_done_task/fetcher.rb
+++ app/models/all_done_task/fetcher.rb
@@ -10,20 +10,21 @@ class AllDoneTask::Fetcher
     list_id = ENV['TRELLO_DONE_LIST_ID']
     key = ENV['TRELLO_API_KEY']
     token = ENV['TRELLO_API_TOKEN']
-    get_cards_uri = '/lists/' + list_id + '/cards?fields=name&key=' + key + '&token=' + token
-    res = Net::HTTP.get(URI.parse(url + get_cards_uri))
+    cards_uri = "/lists/#{list_id}/cards?fields=name&key=#{key}&token=#{token}"
+    res = Net::HTTP.get(URI.parse(url + cards_uri))
     @cards = JSON.parse(res)
   end
 
   def save_all
     AllDoneTask.transaction do
       @cards.each do |card|
-        AllDoneTask.create!(confirmed_on: Time.zone.today, card_code: card['id'], card_name: card['name'])
+        AllDoneTask.create!(confirmed_on: Time.zone.today,
+                            card_code: card['id'], card_name: card['name'])
       end
     end
     return true
-    rescue => ex
-      errors[:base] << ex.message
-      return false
+  rescue => ex
+    errors[:base] << ex.message
+    return false
   end
 end
