4cf1d76cb820d73148ebf44534a07ab66747d3f3
summer-snow <pig.book.notice@gmail.com>
2017-08-27 05:08:22 +0900

    Modify following rubocop rules

--- app/models/done_task.rb
+++ app/models/done_task.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class DoneTask < ApplicationRecord
   validates :end_on, :card_name, presence: true
 end
--- spec/factories/done_tasks.rb
+++ spec/factories/done_tasks.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 FactoryGirl.define do
   factory :done_task do
     end_on Time.zone.yesterday
