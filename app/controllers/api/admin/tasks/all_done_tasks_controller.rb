10bbff094e043ce4fa96aaa4d0b05beac59e81e8
summer-snow <pig.book.notice@gmail.com>
2017-08-28 00:34:55 +0900

    Use list_id

--- app/controllers/api/admin/tasks/all_done_tasks_controller.rb
+++ app/controllers/api/admin/tasks/all_done_tasks_controller.rb
@@ -5,11 +5,17 @@ class Api::Admin::Tasks::AllDoneTasksController < ApplicationController
   before_action :admin_authenticate
 
   def create
-    fetcher = AllDoneTask::Fetcher.new
+    fetcher = AllDoneTask::Fetcher.new(task_params)
     if fetcher.save_all
       head 201
     else
       render_error fetcher
     end
+  end
+
+  private
+
+  def task_params
+    params.permit(:list_id)
   end
 end
--- app/models/all_done_task/fetcher.rb
+++ app/models/all_done_task/fetcher.rb
@@ -5,9 +5,9 @@ require 'net/http'
 class AllDoneTask::Fetcher
   include ActiveModel::Model
 
-  def initialize
+  def initialize(task_params)
+    list_id = task_params['list_id']
     url = ENV['TRELLO_URL']
-    list_id = ENV['TRELLO_DONE_LIST_ID']
     key = ENV['TRELLO_API_KEY']
     token = ENV['TRELLO_API_TOKEN']
     cards_uri = "/lists/#{list_id}/cards?fields=name&key=#{key}&token=#{token}"
@@ -17,6 +17,7 @@ class AllDoneTask::Fetcher
 
   def save_all
     AllDoneTask.transaction do
+      AllDoneTask.where(confirmed_on: Time.zone.today).delete_all
       @cards.each do |card|
         AllDoneTask.create!(confirmed_on: Time.zone.today,
                             card_code: card['id'], card_name: card['name'])
