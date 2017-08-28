59c93557a6850ac4426d536884c9eeaf2c9d3a30
summer-snow <pig.book.notice@gmail.com>
2017-08-28 12:54:37 +0900

    Modify the all done task fetcher methods

--- app/models/all_done_task.rb
+++ app/models/all_done_task.rb
@@ -2,4 +2,14 @@
 
 class AllDoneTask < ApplicationRecord
   validates :confirmed_on, :list_code, :card_code, :card_name, presence: true
+
+  # 確認日「今日」のDoneカードを再登録する
+  def self.recreate_all!(list_code, cards)
+    AllDoneTask.where(confirmed_on: Time.zone.today, list_code: list_code)
+               .delete_all
+    cards.each do |card|
+      AllDoneTask.create!(confirmed_on: Time.zone.today, list_code: list_code,
+                          card_code: card['id'], card_name: card['name'])
+    end
+  end
 end
--- app/models/all_done_task/fetcher.rb
+++ app/models/all_done_task/fetcher.rb
@@ -6,22 +6,18 @@ class AllDoneTask::Fetcher
   include ActiveModel::Model
 
   def initialize(task_params)
-    list_id = task_params['list_id']
+    @list_id = task_params['list_id']
     url = ENV['TRELLO_URL']
     key = ENV['TRELLO_API_KEY']
     token = ENV['TRELLO_API_TOKEN']
-    cards_uri = "/lists/#{list_id}/cards?fields=name&key=#{key}&token=#{token}"
+    cards_uri = "/lists/#{@list_id}/cards?fields=name&key=#{key}&token=#{token}"
     res = Net::HTTP.get(URI.parse(url + cards_uri))
     @cards = JSON.parse(res)
   end
 
   def save_all
     AllDoneTask.transaction do
-      AllDoneTask.where(confirmed_on: Time.zone.today).delete_all
-      @cards.each do |card|
-        AllDoneTask.create!(confirmed_on: Time.zone.today,
-                            card_code: card['id'], card_name: card['name'])
-      end
+      AllDoneTask.recreate_all!(@list_id, @cards)
     end
     true
   rescue => ex
