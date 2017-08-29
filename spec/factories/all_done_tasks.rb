95b2597e8ab61419641880c669ec8cac0aed25d1
summer-snow <pig.book.notice@gmail.com>
2017-08-30 02:02:59 +0900

    Modify the factory of all done tasks

--- spec/factories/all_done_tasks.rb
+++ spec/factories/all_done_tasks.rb
@@ -3,7 +3,9 @@
 FactoryGirl.define do
   factory :all_done_task do
     confirmed_on Time.zone.today
+    list_code SecureRandom.hex(12)
     card_code SecureRandom.hex(12)
     sequence(:card_name) { |i| "カードの名前#{i}" }
+    sequence(:board_name) { |i| "ボードの名前#{i}" }
   end
 end
