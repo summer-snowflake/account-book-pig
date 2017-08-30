224851933a81b2a7bdfd77e053ac6ff96a7ed40c
summer-snow <pig.book.notice@gmail.com>
2017-08-30 18:06:22 +0900

    Add board_name

--- app/models/done_task.rb
+++ app/models/done_task.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 class DoneTask < ApplicationRecord
-  validates :end_on, :card_name, presence: true
+  validates :end_on, :board_name, :card_name, presence: true
 end
