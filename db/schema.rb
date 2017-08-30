ab94d6bd5591e4fce3a58c141212034bf86aa3cd
summer-snow <pig.book.notice@gmail.com>
2017-08-30 18:06:09 +0900

    Add column board_name to done tasks table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170828091246) do
+ActiveRecord::Schema.define(version: 20170830090535) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -112,6 +112,7 @@ ActiveRecord::Schema.define(version: 20170828091246) do
     t.string "card_name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "board_name", null: false
   end
 
   create_table "feedbacks", id: :serial, force: :cascade do |t|
