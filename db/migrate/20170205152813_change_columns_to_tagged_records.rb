11afcdd2ca09ea27a6280f07c0c61aecef02edbc
kaekasui <kaekasui_github@icloud.com>
2018-05-04 02:27:07 +0900

    Change the column to tag_id null true

--- db/migrate/20170205152813_change_columns_to_tagged_records.rb
+++ db/migrate/20170205152813_change_columns_to_tagged_records.rb
@@ -1,13 +1,13 @@
 class ChangeColumnsToTaggedRecords < ActiveRecord::Migration[5.0]
   def up
-    change_column :tagged_records, :tag_id, :integer, null: false, index: true
+    change_column :tagged_records, :tag_id, :integer, index: true
     change_column :tagged_records, :record_id, :integer, null: false, index: true
 
     add_index :tagged_records, [:record_id, :tag_id], unique: true
   end
 
   def down
-    change_column :tagged_records, :tag_id, :integer, null: true
+    change_column :tagged_records, :tag_id, :integer
     change_column :tagged_records, :record_id, :integer, null: true
 
     remove_index :tagged_records, [:record_id, :tag_id]
